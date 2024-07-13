2053a15c4b4c4e97591db1b64d4817def7e3b328
kaveesha150 <kumarijayamanike39@gmail.com>
2024-07-13 13:38:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94789284143' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'zBcQgK4a#442_tdwq1KSd1DsEtB8dmtsqefgKv2YLFcZ8rV6RQqc' ,
+SESSION_ID: process.env.SESSION_ID || 'qNkUWJaA#iPe_o7GX7rVnLOqMXTKZVATF9WaxWvykWzpwMGCBpAA' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
